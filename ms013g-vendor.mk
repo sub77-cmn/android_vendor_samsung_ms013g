7bcef401544e77d862aa05c00a66224709229022
Apavayan Sinha <info@apavayan.com>
2015-11-13 21:12:00 +0530

    ms013g: vendor clean up

--- ms013g-vendor.mk
+++ ms013g-vendor.mk
@@ -16,9 +16,8 @@
 
 # Pick up overlay for features that depend on non-open-source files
 PRODUCT_PACKAGES += \
-	libtime_genoff \
-	libHevcSwDecoder \
-	libTimeService \
-	TimeService
+    com.qualcomm.location \
+    libHevcSwDecoder \
+    TimeService
 
 $(call inherit-product, vendor/samsung/ms013g/ms013g-vendor-blobs.mk)
